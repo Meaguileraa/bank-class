afd4429d3bbec1595ac278b26d8c864cf9be1660
Marissa Aguilera <marissaguilera@yahoo.com>
2020-11-08 21:49:44 -0800

    Bank class

--- solution.py
+++ solution.py
@@ -1,3 +1,18 @@
 
 #Create a b ank class that has withdrawal and deposit methods 
 #create a MinimumBalanceAccount class that inherits from the bank class 
+
+
+class Bank:
+    """A bank."""
+
+    def __init__(self):
+        """Initializing."""
+
+        self.balance = 0
+
+    def withdrawal(self, amount):
+        """Taking money out of the bank."""
+
+        balance -= amount
+        return self.balance
