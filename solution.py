c9397aca02fc3ee3bea4535517d3806df24a2eb6
Marissa Aguilera <marissaguilera@yahoo.com>
2020-11-08 21:55:45 -0800

    Test cases

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@
 #create a MinimumBalanceAccount class that inherits from the bank class 
 
 
-class Bank:
+class BankAccount:
     """A bank."""
 
     def __init__(self):
@@ -12,7 +12,24 @@ class Bank:
         self.balance = 0
 
     def withdrawal(self, amount):
-        """Taking money out of the bank."""
+        """Taking money out of my account."""
 
-        balance -= amount
+        self.balance -= amount
         return self.balance
+
+    def deposit(self, amount):
+        """Putting money into my account."""
+
+        self.balance += amount 
+        return self.balance
+
+#TEST CASES
+
+a = BankAccount()
+b = BankAccount()
+
+print(a.deposit(100))
+print(b.deposit(1000))
+
+print(a.withdrawal(30))
+print(b.withdrawal(250))
